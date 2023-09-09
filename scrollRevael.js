fa0bab67c9b190aa8c0646597d5a6a5af3ca63e6
Samuel Jardim Alves <132871256+Samuelgardenn@users.noreply.github.com>
2023-09-08 23:07:24 -0300

    Add files via upload

---
+++ scrollRevael.js
@@ -0,0 +1,11 @@
+window.sr = ScrollReveal({reset: true})
+
+ScrollReveal().reveal('.scroll-1',{
+    delay: 400
+});
+ScrollReveal().reveal('.scroll-2',{
+    delay: 500
+});
+ScrollReveal().reveal('.scroll-3',{
+    delay:600
+});
